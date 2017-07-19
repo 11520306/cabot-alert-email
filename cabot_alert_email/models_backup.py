0f45c80304232cbf1e9fff081e0d682889243211
QuangNguyen <anhquangnet@gmail.com>
2017-07-19 14:14:43 +0700

    Update template HAProxy

--- cabot_alert_email/models_backup.py
+++ cabot_alert_email/models_backup.py
@@ -15,19 +15,12 @@ email_template = """Service {{ service.name }} {{ scheme }}://{{ host }}{% url '
 CHECKS FAILING:{% for check in service.all_failing_checks %}
   FAILING - {{ check.name }} - Metric: {{ check.metric }} - Value:  {{ check.last_result.error|safe }} {% endfor %}
 {% if service.all_passing_checks %}
-PASSING CHECKS:{% for check in service.all_passing_checks %}
+Passing checks:{% for check in service.all_passing_checks %}
   PASSING - {{ check.name }} - Metric: {{ check.metric }} - Value: {{ check.last_result.error|safe }} - OK {% endfor %}
 {% endif %}
 {% endif %}
 """
 
-email_template_haproxy = """HAService {{ service.name }} {{ scheme }}://{{ host }}{% url 'service' pk=service.id %} 
-{% if service.overall_status != service.PASSING_STATUS %}alerting with status: {{ service.overall_status }}{% else %}is back to normal{% endif %}.
-{% if service.overall_status != service.PASSING_STATUS %}
-CHECKS FAILING:{% for check in service.all_failing_checks %}
-  SERVICE FAILING - {{ check.name }} - NODE: {{ check.last_result.error|safe }} {% endfor %}
-{% endif %}
-"""
 
 class EmailAlert(AlertPlugin):
     name = "Email"
@@ -38,12 +31,10 @@ class EmailAlert(AlertPlugin):
         emails = [u.email for u in users if u.email]
         if not emails:
             return
-        resp = "Hello World NA"
         c = Context({
             'service': service,
             'host': settings.WWW_HTTP_HOST,
-            'scheme': settings.WWW_SCHEME,
-            'resp': resp
+            'scheme': settings.WWW_SCHEME
         })
         if service.overall_status != service.PASSING_STATUS:
             for check in service.all_failing_checks():
@@ -52,18 +43,14 @@ class EmailAlert(AlertPlugin):
             if service.overall_status == service.CRITICAL_STATUS:
                 emails += [u.email for u in users if u.email]
             # subject = '%s status for service: %s *** | %s ***' % (
-            subject = '[Pateco Alerts] Service [%s] %s *** | %s *** ' % (
+            subject = '[Pateco Alerts] [%s] %s *** | %s *** ' % (
                 service.name, service.overall_status, alltype)
         else:
             for check in service.all_passing_checks():
                 alltype += str(check.name)
                 alltype += " | "
-            subject = '[Pateco Alerts] Service [%s] OK' % (service.name)
-
-        if service.name == "HAProxy":
-            t = Template(email_template_haproxy)
-        else:        
-            t = Template(email_template)
+            subject = '[Pateco Alerts] [%s] OK *** | %s ***' % (service.name, alltype)
+        t = Template(email_template)
         send_mail(
             subject=subject,
             message=t.render(c),
